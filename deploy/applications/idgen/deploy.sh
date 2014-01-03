cf2d8d36e40e0065f29c3768fd928f51521d7569
Kai Kewley <kaikewley@gmail.com>
2014-01-03 11:36:35 +0000

    Create config branch. Deploy script which applies config. Can deploy idgen app.

---
+++ deploy/applications/idgen/deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+config_dir=$1
+tmp="tmp"
+
+webapps="/var/lib/tomcat7/webapps"
+axis_url="http://apache.mirrors.timporter.net//axis/axis2/java/core/1.6.2/axis2-1.6.2-war.zip"
+axis_zip="axis2.zip"
+axis_war="axis2.war"
+
+aar_url="http://build.snomedtools.org/job/snomed-release-system/ws/id-generation/id-generation-ws/target/idgen.aar"
+aar_file="idgen.aar"
+axis_services="/var/lib/tomcat7/webapps/axis2/WEB-INF/services/"
+
+
+echo "Checking Axis2 war is installed"
+if [ ! -f $webapps/axis2.war ]; then
+	echo "Downloading Axis2 war"
+	wget $axis_url -O $axis_zip
+	unzip $axix_zip $axis_war
+	mv $axis_war $webapps
+else
+	echo "Axis2 is there"
+fi
+
+echo "Downloading latest $aar_file"
+wget $aar_url
+
+if [ "$config_dir" != "" ]; then
+	echo "Appying config"
+	rm -rf config
+	cp -r ../$config_dir config
+	cd config
+	zip -r ../$aar_file *
+	cd ..
+else
+	echo "No config to apply"
+fi
+
+mv $aar_file $axis_services
---
+++ deploy/deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+cd $(dirname "$0");
+pwd
+
+# Check params
+if [ $# -eq 1 ]; then
+
+	app=$1
+	host=`hostname`
+
+	echo "Choosing $app config for $host"
+	config_dir="hosts/${host}/${app}"
+	if [ -d $config_dir ]; then
+		echo "Found host specific config for this app."
+	else
+		config_dir="host/*/${app}"
+		if [ -d $config_dir ]; then
+			echo "Found generic host config for this app."
+		else
+			config_dir=""
+			echo "No config overrides found for this app."
+		fi
+	fi
+
+	deploy_script="applications/${app}/deploy.sh"
+	tmp="tmp"
+	if [ -f $deploy_script ]; then
+		rm -rf $tmp
+		mkdir $tmp
+		cd $tmp
+		../$deploy_script $config_dir
+		cd -
+		rm -rf $tmp
+	else
+		echo "Deploy script for application '$app' not found."
+	fi
+
+else
+
+	echo "Usage: ./deploy.sh [app_name]"
+	exit 1
+fi
